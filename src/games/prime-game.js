78e2ff5c41eff8382ad37e06c7b904d1ea9f800e
Rodion Kondakov <wead758@gmail.com>
2024-01-21 09:05:15 +0300

    added prime game

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -1,23 +1,27 @@
 import { greetPlayer, getRandomNumber, getUserAnswer } from '../index.js';
 
-function isEven(number) {
-  return number % 2 === 0;
+function isPrime(number) {
+  if (number <= 1) return false;
+  for (let i = 2; i * i <= number; i += 1) {
+    if (number % i === 0) return false;
+  }
+  return true;
 }
 
 function askQuestion() {
-  const question = getRandomNumber();
+  const question = getRandomNumber() * 7;
   console.log(`Question: ${question}`);
   return question;
 }
 
 function checkAnswer(question, userAnswer) {
-  const correctAnswer = isEven(question) ? 'yes' : 'no';
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
   return userAnswer === correctAnswer;
 }
 
 function startGame() {
   const playerName = greetPlayer();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 
   for (let i = 0; i < 3; i += 1) {
     const question = askQuestion();
@@ -27,7 +31,7 @@ function startGame() {
       console.log('Correct!');
     } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${
-        isEven(question) ? 'yes' : 'no'
+        isPrime(question) ? 'yes' : 'no'
       }'.
             Let's try again, ${playerName}`);
       return;
