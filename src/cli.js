4a66facfc3f91a4717d39b5677159bd7491e02bb
Rodion Kondakov <wead758@gmail.com>
2024-01-23 08:23:04 +0300

    another brain-game fixes

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,10 @@
 import readlineSync from 'readline-sync';
 
-const askName = () => readlineSync.question('May I have your name?: ');
-
-function getUserAnswer() {
-  return readlineSync.question('Your answer: ');
+function greetings() {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${userName}!`);
+  return userName;
 }
 
-export { askName, getUserAnswer };
+export default greetings;
